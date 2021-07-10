135fa1afa2173b4c57878f28331232501a271e11
Alex Ladosha <willost17@gmail.com>
2021-07-10 13:11:21 +0300

    New2

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\UserController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('admin/index');
-});
+})->name('home');
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/', [MainController::class, 'index'])->name('admin.index');
@@ -18,6 +19,13 @@ Route::group(['prefix' => 'admin'], function () {
     Route::resource('/tags', TagController::class);
     Route::resource('/posts', PostController::class);
 });
+
+Route::get('/register', [UserController::class, 'create'])->name('user.create');
+Route::post('/register', [UserController::class, 'store'])->name('user.store');
+Route::get('/login', [UserController::class, 'loginForm'])->name('login.create');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
 
 
 
